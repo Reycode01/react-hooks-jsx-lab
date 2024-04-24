1d8be9df29f0c46af4ba252f7c1ad0d1e91a1679
humphrey <writernic89@gmail.com>
2024-04-25 00:09:32 +0300

    add jsx lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,12 @@
-import React from "react";
-import { name, city } from "../data/data.js";
+import React from 'react';
 
-function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+const Home = ({ name, city }) => {
+  return (
+    <div id="home">
+      <h1 style={{ color: 'firebrick' }}>{name}Liza is a Web Developer from {city} New York</h1>
+    </div>
+  );
 }
 
 export default Home;
+
